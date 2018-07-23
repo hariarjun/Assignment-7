5ac1cae452faf5787c8cde733eb19ad985a04dfc
DASARI HAREESH <39777720+hariarjun@users.noreply.github.com>
2018-07-24 01:12:21 +0530

    Add files via upload

---
+++ Assignment 7.1.r
@@ -0,0 +1,20 @@
+#Question 1. Histogram for all variables in a dataset mtcars. Write a program to create histograms for all columns.
+
+library(purrr)
+library(tidyr)
+library(ggplot2)
+
+mtcars %>% keep(is.numeric) %>% gather() %>% ggplot(aes(value)) + facet_wrap(~ key,scales = "free") + geom_histogram()
+
+#Question 2. Check the probability distribution of all variables in mtcars
+
+library(ggplot2)
+
+mtcars %>% keep(is.numeric) %>% gather() %>% ggplot(aes(value)) + facet_wrap(~ key,scales = "free") + stat_density()
+
+#Question 3. Write a program to create boxplot for all variables.
+
+library(ggplot2)
+library(reshape)
+m1 <- melt(mtcars)
+ggplot(m1,aes(x = variable,y = value)) + facet_wrap(~variable) + geom_boxplot()
---
+++ Assignment 7.3.r
@@ -0,0 +1,15 @@
+#Question 1: Create a box and whisker plot by class using mtcars dataset.
+
+#Method 1:
+library(ggplot2)
+#using a different color for each group
+ggplot(mpg, aes(x=class, y=hwy,fill=class, font.main=3)) +  
+  ggtitle(toupper("Different Classes")) +
+  geom_boxplot(alpha=0.3) +theme(legend.position="none")
+
+
+#Method 2:
+boxplot(mpg~cyl, data=mtcars, 
+        main= toupper("Fuel Consumption"), font.main=3, 
+        col= topo.colors(3), xlab="Number of Cylinders", 
+        ylab="Miles per Gallon")
